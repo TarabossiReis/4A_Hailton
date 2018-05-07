44bb5050311c7fb67b398a1e4f4b42f2579f8f69
03828184235 <03828184235@JIPA-LABIN05-25.ifro.local>
2018-05-07 09:00:29 -0400

    Atualização

--- Grupo1/src/br/edu/ifro/MenuPrincipalController.java
+++ Grupo1/src/br/edu/ifro/MenuPrincipalController.java
@@ -10,23 +10,37 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 /**
  *
  * @author 03828184235
  */
 public class MenuPrincipalController implements Initializable {
+
+    @FXML
+    private TextField txtNumero1;
+    @FXML
+    private TextField txtNumero2;
+    @FXML
+    private TextField txtResultado;
     
     @FXML
-    private Label label;
+    private Button btnSomar;
+    
+
     
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    private void soma (ActionEvent event) {
+        Double num1 = Double.parseDouble(txtNumero1.getText());
+        Double num2 = Double.parseDouble(txtNumero2.getText());
+        Double resultado = num1 + num2;
+        
+        txtResultado.setText(resultado.toString());
     }
-    
+        
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
